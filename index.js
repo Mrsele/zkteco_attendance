da48f740566e4b702a00f15fbe37220c8fe9b826
Solomon Yeshiwas <121292969+Mrsele@users.noreply.github.com>
2024-10-02 16:10:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ZKLib = require('./zklib')
 
 const test = async () => {
-  // Replace '123' with your actual commkey if needed
+
   let zkInstance = new ZKLib('10.10.1.26', 4370, 10000, 4000,123);
   
   try {
@@ -13,24 +13,21 @@ const test = async () => {
     const users = await zkInstance.getUsers();
     console.log('Users:', users);
 
-   // Fetch attendance logs
     console.log('Fetching attendance logs...');
     const logs = await zkInstance.getAttendances();
     console.log('Attendance Logs:', logs);
 
-    // Real-time logs callback
     zkInstance.getRealTimeLogs((data) => {
         console.log('Real-time log:', data);
     });
 
-    // Clear attendance logs if needed
     // console.log('Clearing attendance logs...');
     // await zkInstance.clearAttendanceLog();
 
   } catch (error) {
     console.error('Error:', error);
   } finally {
-    // Disconnect from the device
+  
     try {
       console.log('Disconnecting from device...');
       await zkInstance.disconnect();
